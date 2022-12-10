722a4bbd39b0a787cc0667cc5ac597c2accf2f29
semjon <simon.gerasimov@gmail.com>
2022-12-10 12:06:56 +0300

    Initial Commit

---
+++ OnlineShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineShop
+//
+//  Created by mac on 10.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
